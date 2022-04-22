f422239f4c5c0ba9c9b74b1cd28ba9a408027db7
rgc99 <rgc99@users.noreply.github.com>
2022-04-22 02:49:11 +0000

    Update files

--- rollup.config.js
+++ rollup.config.js
@@ -6,9 +6,6 @@ import { terser } from 'rollup-plugin-terser';
 import serve from 'rollup-plugin-serve';
 import json from '@rollup/plugin-json';
 import ignore from './rollup-plugins/ignore';
-import { ignoreTextfieldFiles } from './elements/ignore/textfield';
-import { ignoreSelectFiles } from './elements/ignore/select';
-import { ignoreSwitchFiles } from './elements/ignore/switch';
 
 const dev = process.env.ROLLUP_WATCH;
 
@@ -32,9 +29,6 @@ const plugins = [
   }),
   dev && serve(serveopts),
   !dev && terser(),
-  ignore({
-    files: [...ignoreTextfieldFiles, ...ignoreSelectFiles, ...ignoreSwitchFiles].map((file) => require.resolve(file)),
-  }),
 ];
 
 export default [
