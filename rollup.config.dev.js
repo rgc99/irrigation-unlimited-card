bcc6b873d13ebd91d7564d59b00cf249d6ff7e2f
Robert Cook <rgc99@github.com>
2025-12-15 03:43:02 +0000

    Update dev environment

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,5 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import babel from "@rollup/plugin-babel";
 import serve from "rollup-plugin-serve";
 import json from "@rollup/plugin-json";
